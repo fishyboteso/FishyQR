6c90a4962c8a3a4b7bb6d0d4fc606c17789484ec
Semjon Kerner <Semjon.Kerner@gmx.net>
2020-12-31 13:48:21 +0100

    add TODO, fix positioning

--- EsoQR.lua
+++ EsoQR.lua
@@ -4,26 +4,39 @@ local logger = LibDebugLogger(EsoQR.name)
 logger:Warn("RUN")
 
 local debugbool = true
-local pixelsize = 2
+local pixelsize = 5
 local maxpixels = 25
 gps = LibStub("LibGPS2")
 
-local function _test(eventCode, bookTitle, body, medium, showTitle, bookId)
-    logger:Warn(bookTitle)
-    local x, y, zoneMapIndex = gps:LocalToGlobal(GetMapPlayerPosition("player"))
-    local angle = (math.deg(GetPlayerCameraHeading())-180) % 360
-    localization = string.format("%f : %f : %d", x, y, angle)
+--[[
+0.  add button
+1.  register event on button click (set_run_var_true())
+1.1     unregister event on button click (set_run_var_true())
+1.2     register event on button click (set_run_var_false())
+1.3     set running_var=true
+1.4     call qrcode-generation-function
+2   generate qrcode in a loop
+2.1     get Chalutier state, Keybindings, GPS
+2.2     end qrcode-generation-function when running_var=false
 
-    local ok, tab_or_message = qrcode(localization) --THIS IS AN IMPORTANT LINE
+- always in front
+]]--
+
+local function _test(eventCode, bookTitle, body, medium, showTitle, bookId)
+    --logger:Warn(bookTitle)
+    local x, y, zoneMapIndex = gps:LocalToGlobal(GetMapPlayerPosition("player"))
+    --local angle = (math.deg(GetPlayerCameraHeading())-180) % 360
+    --localization = string.format("%f : %f : %d", x, y, angle)
+
+    local ok, tab_or_message = qrcode(bookTitle) --THIS IS AN IMPORTANT LINE
     
     if not ok then
         logger:Warn("is NOT ok")
     else
         logger:Warn("is ok")
-        --logger:Warn(LibTableFunctions:PrintTable(tab_or_message))
         --blank
-        for r=0,24 do
-            for c=0,24 do
+        for r=0,maxpixels-1 do
+            for c=0,maxpixels-1 do
                 EsoQR.UI.pixel[r][c]:SetHidden(true)
             end
         end
@@ -58,9 +71,9 @@ function EsoQR.OnAddOnLoaded(event, addonName)
         EsoQR.UI.background = WINDOW_MANAGER:CreateControl(nil, EsoQR.UI, CT_TEXTURE)
         EsoQR.UI.background:SetDimensions(4 + maxpixels*pixelsize + 4, 4 + maxpixels*pixelsize + 4) -- 4 + 25*2 + 4
         EsoQR.UI.background:SetColor(1, 1, 1)
-        EsoQR.UI.background:SetAnchor(TOP, EsoQR.UI, TOP, 0, background)
+        EsoQR.UI.background:SetAnchor(TOPLEFT, EsoQR.UI, TOPLEFT, 0, 0)
         EsoQR.UI.background:SetHidden(false)
-        EsoQR.UI.background:SetDrawLevel(1)
+        EsoQR.UI.background:SetDrawLevel(0)
         
         EsoQR.UI.pixel = {}
         for i = 0,24 do
@@ -71,7 +84,7 @@ function EsoQR.OnAddOnLoaded(event, addonName)
                 EsoQR.UI.pixel[i][j]:SetColor(0, 0, 0)
                 EsoQR.UI.pixel[i][j]:SetAnchor(TOPLEFT, EsoQR.UI.background, TOPLEFT, 4+(i*pixelsize), 4+(j*pixelsize))
                 EsoQR.UI.pixel[i][j]:SetHidden(true)
-                EsoQR.UI.pixel[i][j]:SetDrawLevel(2)
+                EsoQR.UI.pixel[i][j]:SetDrawLevel(1)
             end
         end
         
