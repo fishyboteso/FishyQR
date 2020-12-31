5a438c8d26fce59e1306adbaeded9ebb650bd2ee
Semjon Kerner <Semjon.Kerner@gmx.net>
2020-12-31 20:00:39 +0100

    add addon menu and saved vars

--- EsoQR.lua
+++ EsoQR.lua
@@ -7,9 +7,12 @@ EsoQR = {
 local gps = LibStub("LibGPS2")
 
 --PARAMS:
-local pixelsize = 3
-local maxpixels = 25
-local updatetime = 400
+local EsoQRparams = {}
+local EsoQRdefaults = {
+    pixelsize = 3,
+    maxpixels = 25,
+    updatetime = 400
+}
 
 --[[
 [X] 0.  add button
@@ -32,8 +35,8 @@ TODO:
 
 --make qr blank
 local function _blankQR()
-    for row=0,maxpixels-1 do
-        for col=0,maxpixels-1 do
+    for row=0,EsoQRparams.maxpixels-1 do
+        for col=0,EsoQRparams.maxpixels-1 do
             EsoQR.UI.pixel[row][col]:SetHidden(true)
         end
     end
@@ -57,7 +60,7 @@ end
 
 local function _generateQR()
     EVENT_MANAGER:UnregisterForUpdate(EsoQR.name)
-    local updatetime_ms = updatetime
+    local updatetime_ms = EsoQRparams.updatetime
     
     --get the gps values and form them to a string
     local x, y, zoneMapIndex = gps:LocalToGlobal(GetMapPlayerPosition("player"))
@@ -90,6 +93,7 @@ local function _toggle_running_state()
         EsoQR.UI.button:SetMouseOverTexture(EsoQR.name .. "/img/start_mouseover.dds")
     else
         EsoQR.run_var = true
+        EsoQR.oldlocal = ""
         EsoQR.UI.button:SetNormalTexture(EsoQR.name .. "/img/start_running.dds")
         EsoQR.UI.button:SetMouseOverTexture(EsoQR.name .. "/img/start_running.dds")
         _generateQR()
@@ -98,8 +102,13 @@ end
 
 function EsoQR.OnAddOnLoaded(event, addonName)
     if addonName == EsoQR.name then
+        --init once and never come here again
         EVENT_MANAGER:UnregisterForEvent(EsoQR.name, EVENT_ADD_ON_LOADED)
         
+        --load settings
+        EsoQRparams = ZO_SavedVars:NewAccountWide("EsoQRparamsvar", 1, nil, EsoQRdefaults)
+        
+        --create qr ui code elements
         EsoQR.UI = WINDOW_MANAGER:CreateControl(nil, GuiRoot, CT_TOPLEVELCONTROL)
         EsoQR.UI:SetMouseEnabled(true)
         EsoQR.UI:SetClampedToScreen(true)
@@ -110,25 +119,26 @@ function EsoQR.OnAddOnLoaded(event, addonName)
         EsoQR.UI:SetDrawTier(0)
         
         EsoQR.UI.background = WINDOW_MANAGER:CreateControl(nil, EsoQR.UI, CT_TEXTURE)
-        EsoQR.UI.background:SetDimensions(4 + maxpixels*pixelsize + 4, 4 + maxpixels*pixelsize + 4) -- 4 + 25*2 + 4
+        EsoQR.UI.background:SetDimensions(4 + EsoQRparams.maxpixels*EsoQRparams.pixelsize + 4, 4 + EsoQRparams.maxpixels*EsoQRparams.pixelsize + 4) -- 4 + 25*2 + 4
         EsoQR.UI.background:SetColor(1, 1, 1)
         EsoQR.UI.background:SetAnchor(TOPLEFT, EsoQR.UI, TOPLEFT, 0, 0)
         EsoQR.UI.background:SetHidden(false)
         EsoQR.UI.background:SetDrawLevel(0)
         
         EsoQR.UI.pixel = {}
-        for i = 0,24 do
+        for i = 0,EsoQRparams.maxpixels-1 do
             EsoQR.UI.pixel[i] = {}
-            for j = 0,24 do
+            for j = 0,EsoQRparams.maxpixels-1 do
                 EsoQR.UI.pixel[i][j] = WINDOW_MANAGER:CreateControl(nil, EsoQR.UI, CT_TEXTURE)
-                EsoQR.UI.pixel[i][j]:SetDimensions(pixelsize, pixelsize)
+                EsoQR.UI.pixel[i][j]:SetDimensions(EsoQRparams.pixelsize, EsoQRparams.pixelsize)
                 EsoQR.UI.pixel[i][j]:SetColor(0, 0, 0)
-                EsoQR.UI.pixel[i][j]:SetAnchor(TOPLEFT, EsoQR.UI.background, TOPLEFT, 4+(i*pixelsize), 4+(j*pixelsize))
+                EsoQR.UI.pixel[i][j]:SetAnchor(TOPLEFT, EsoQR.UI.background, TOPLEFT, 4+(i*EsoQRparams.pixelsize), 4+(j*EsoQRparams.pixelsize))
                 EsoQR.UI.pixel[i][j]:SetHidden(true)
                 EsoQR.UI.pixel[i][j]:SetDrawLevel(1)
             end
         end
         
+        --add start button
         EsoQR.UI.button =  WINDOW_MANAGER:CreateControl(EsoQR.name .. "button", ZO_ChatWindow, CT_BUTTON)
         EsoQR.UI.button:SetDimensions(20, 20)
         EsoQR.UI.button:SetAnchor(TOPLEFT, ZO_ChatWindowNotifications, TOPRIGHT, 75, 5)
@@ -137,7 +147,61 @@ function EsoQR.OnAddOnLoaded(event, addonName)
         EsoQR.UI.button:SetMouseOverTexture(EsoQR.name .. "/img/start_mouseover.dds")
         EsoQR.run_var = false
         EsoQR.UI.button:SetHandler("OnClicked", _toggle_running_state)
-                
+
+        local logger = LibDebugLogger("MyAddon")
+
+        --addon menu
+        local LAM = LibAddonMenu2
+        local panelName = EsoQR.name .. "Settings" -- TODO the name will be used to create a global variable, pick something unique or you may overwrite an existing variable!
+         
+        local panelData = {
+            type = "panel",
+            name = EsoQR.name .. " Settings",
+            author = "Semjon Kerner",
+        }
+        local panel = LAM:RegisterAddonPanel(panelName, panelData)
+        local optionsData = {
+            --[[
+            {
+                type = "slider",
+                name = "Maximum Pixels",
+                min = 22,
+                max = 30,
+                default = 25,
+                getFunc = function() return params.maxpixels end,
+                setFunc = function(value) params.maxpixels = maxpixels end
+            },
+            ]]--
+            {
+                type = "slider",
+                name = "Pixel Size",
+                min = 1,
+                max = 10,
+                default = 2,
+                getFunc = function() return EsoQRparams.pixelsize end,
+                setFunc = function(value)
+                    EsoQRparams.pixelsize = value
+                    EsoQR.UI.background:SetDimensions(4 + EsoQRparams.maxpixels*EsoQRparams.pixelsize + 4, 4 + EsoQRparams.maxpixels*EsoQRparams.pixelsize + 4) -- 4 + 25*2 + 4
+                    for i = 0,EsoQRparams.maxpixels-1 do
+                        for j = 0,EsoQRparams.maxpixels-1 do
+                            EsoQR.UI.pixel[i][j]:SetDimensions(EsoQRparams.pixelsize, EsoQRparams.pixelsize)
+                            EsoQR.UI.pixel[i][j]:SetAnchor(TOPLEFT, EsoQR.UI.background, TOPLEFT, 4+(i*EsoQRparams.pixelsize), 4+(j*EsoQRparams.pixelsize))
+                        end
+                    end
+                end
+            },
+            {
+                type = "slider",
+                name = "Updatetime",
+                min = 100,
+                max = 10000,
+                default = 400,
+                getFunc = function() return EsoQRparams.updatetime end,
+                setFunc = function(value) EsoQRparams.updatetime = value end
+            }
+        }
+        LAM:RegisterOptionControls(panelName, optionsData)
+        
     end
 end
  
