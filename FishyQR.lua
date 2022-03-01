6eaeb0d542757c4bde720003bd6d29487cfc6745
Semjon Kerner <Semjon.Kerner@gmx.net>
2022-03-01 22:46:15 +0100

    add createMenu and createUI functions

--- FishyQR.lua
+++ FishyQR.lua
@@ -16,9 +16,9 @@ local FishyQRdefaults = {
     posy        = 0,
     run_var     = false
 }
+
 local brdr = 10
 local text = 20
-
 local dimX = 0
 local dimY = 0
 
@@ -65,6 +65,7 @@ local function _drawQR(keyString)
     end
 end
 
+
 local tmpKeyString = ""
 local function _generateQR()
     --get the gps values and form them to a string
@@ -79,6 +80,7 @@ local function _generateQR()
     end
 end
 
+
 local function _stopState()
     FishyQR.UI.buttonLabel:SetText("V")
     FishyQR.UI.background:SetDimensions(dimX, text + brdr)
@@ -92,6 +94,7 @@ local function _stopState()
     EVENT_MANAGER:UnregisterForUpdate(FishyQR.name .. "generateQR", FishyQRparams.updatetime, _generateQR)
 end
 
+
 local function _startState()
     FishyQR.UI.background:SetDimensions(dimX, dimY)
     FishyQR.UI.buttonLabel:SetText("—")
@@ -104,6 +107,7 @@ local function _startState()
     EVENT_MANAGER:RegisterForUpdate(FishyQR.name .. "generateQR", FishyQRparams.updatetime, _generateQR)
 end
 
+
 local function _update_state()
     if FishyQR.running then
         _startState()
@@ -112,10 +116,145 @@ local function _update_state()
     end
 end
 
+
 function FishyQR.toggle_running_state()
     FishyQR.running = not FishyQR.running
     _update_state()
 end
+
+
+function _createUI()
+    --create qr ui code elements
+    dimX = brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize + brdr
+    dimY = text + brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize + brdr
+
+    FishyQR.UI = WINDOW_MANAGER:CreateControl(nil, GuiRoot, CT_TOPLEVELCONTROL)
+    FishyQR.UI:SetMouseEnabled(true)
+    FishyQR.UI:SetClampedToScreen(true)
+    FishyQR.UI:SetMovable(true)
+    FishyQR.UI:SetDimensions(dimX, dimY)
+    FishyQR.UI:SetDrawLevel(0)
+    FishyQR.UI:SetDrawLayer(DL_MAX_VALUE-1)
+    FishyQR.UI:SetDrawTier(DT_MAX_VALUE-1)
+
+    FishyQR.UI:ClearAnchors()
+    FishyQR.UI:SetAnchor(TOPRIGHT, GuiRoot, TOPRIGHT, FishyQRparams.posx, FishyQRparams.posy)
+
+    FishyQR.UI.background = WINDOW_MANAGER:CreateControl(nil, FishyQR.UI, CT_TEXTURE)
+    FishyQR.UI.background:SetDimensions(dimX, dimY)
+    FishyQR.UI.background:SetColor(1, 1, 1)
+    FishyQR.UI.background:SetAnchor(TOPLEFT, FishyQR.UI, TOPLEFT, 0, 0)
+    FishyQR.UI.background:SetHidden(false)
+    FishyQR.UI.background:SetDrawLevel(0)
+
+    FishyQR.UI.label = WINDOW_MANAGER:CreateControl(FishyQR.name .. "label", FishyQR.UI, CT_LABEL)
+    FishyQR.UI.label:SetFont("ZoFontChat")
+    FishyQR.UI.label:SetColor(0,0,0)
+    FishyQR.UI.label:SetAnchor(TOP, FishyQR.UI.background, TOP, 0, 0)
+    FishyQR.UI.label:SetText("FishyQR")
+
+    FishyQR.UI.buttonLabel = WINDOW_MANAGER:CreateControl(FishyQR.name .. "buttonLabel", FishyQR.UI, CT_LABEL)
+    FishyQR.UI.buttonLabel:SetFont("ZoFontChat")
+    FishyQR.UI.buttonLabel:SetColor(0,0,0)
+    FishyQR.UI.buttonLabel:SetAnchor(TOPRIGHT, FishyQR.UI.background, TOPRIGHT, -brdr, 0)
+
+    FishyQR.UI.button = WINDOW_MANAGER:CreateControl(FishyQR.name .. "button", FishyQR.UI, CT_BUTTON)
+    FishyQR.UI.button:SetDimensions(text, text)
+    FishyQR.UI.button:SetAnchor(TOPRIGHT, FishyQR.UI.background, TOPRIGHT, -brdr, 0)
+    FishyQR.UI.button:SetHandler("OnClicked", FishyQR.toggle_running_state)
+
+    FishyQR.UI.pixel = {}
+    for i = 0,FishyQRparams.maxpixels-1 do
+        FishyQR.UI.pixel[i] = {}
+        for j = 0,FishyQRparams.maxpixels-1 do
+            FishyQR.UI.pixel[i][j] = WINDOW_MANAGER:CreateControl(nil, FishyQR.UI, CT_TEXTURE)
+            FishyQR.UI.pixel[i][j]:SetDimensions(FishyQRparams.pixelsize, FishyQRparams.pixelsize)
+            FishyQR.UI.pixel[i][j]:SetColor(0, 0, 0)
+            FishyQR.UI.pixel[i][j]:SetAnchor(TOPLEFT, FishyQR.UI.background, TOPLEFT, brdr+(i*FishyQRparams.pixelsize), text+brdr+(j*FishyQRparams.pixelsize))
+            FishyQR.UI.pixel[i][j]:SetHidden(true)
+            FishyQR.UI.pixel[i][j]:SetDrawLevel(0)
+        end
+    end
+
+    FishyQR.UI:SetHandler("OnMoveStop", function()
+        FishyQRparams.posy = FishyQR.UI:GetTop()
+        FishyQRparams.posx = FishyQR.UI:GetRight() - GuiRoot:GetRight()
+    end)
+end
+
+
+function _createMenu()
+    --#region addon menu
+    --addon menu
+    local LAM = LibAddonMenu2
+    local panelName = FishyQR.name .. "Settings"
+
+    local panelData = {
+        type = "panel",
+        name = FishyQR.name .. " Settings",
+        author = FishyQR.author,
+    }
+    local panel = LAM:RegisterAddonPanel(panelName, panelData)
+    local optionsData = {
+        {
+            type = "dropdown",
+            name = "Start State",
+            choices = {"enabled", "disabled"},
+            getFunc = function() if FishyQRparams.run_var then return "enabled" end return "disabled" end,
+            setFunc = function(var)
+                if var == "enabled" then
+                    FishyQRparams.run_var = true
+                else
+                    FishyQRparams.run_var = false
+                end
+            end,
+            tooltip = "If enabled FishyQR will start immediately, when the game is loaded.",
+        },
+        {
+            type = "slider",
+            name = "Pixel Size",
+            min = 1,
+            max = 8,
+            default = 8,
+            getFunc = function() return FishyQRparams.pixelsize end,
+            setFunc = function(value)
+                FishyQRparams.pixelsize = value
+                FishyQR.UI:SetDimensions(2*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize, 3*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize + text)
+                FishyQR.UI.background:SetDimensions(2*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize, 3*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize + text)
+                for i = 0,FishyQRparams.maxpixels-1 do
+                    for j = 0,FishyQRparams.maxpixels-1 do
+                        FishyQR.UI.pixel[i][j]:SetDimensions(FishyQRparams.pixelsize, FishyQRparams.pixelsize)
+                        FishyQR.UI.pixel[i][j]:SetAnchor(TOPLEFT, FishyQR.UI.background, TOPLEFT, brdr+(i*FishyQRparams.pixelsize), brdr+(j*FishyQRparams.pixelsize))
+                    end
+                end
+            end,
+            tooltip = "Set the size of each pixel of the QR Code.",
+            requiresReload = true
+        },
+        {
+            type = "slider",
+            name = "Updatetime",
+            min = 150,
+            max = 1500,
+            step = 50,
+            default = 500,
+            getFunc = function() return FishyQRparams.updatetime end,
+            setFunc = function(value) FishyQRparams.updatetime = value end,
+            tooltip = "Set the wait time between each QR Code Update in ms.",
+            requiresReload = true
+        },
+        {
+            type = "description",
+            title = "NOTE",
+            text = "If you experience problems with performance, try increasing Updatetime. The higher the value of Updatetime is, the less it will draw performance.",
+            width = "full"
+        }
+    }
+    LAM:RegisterOptionControls(panelName, optionsData)
+    ZO_CreateStringId("SI_BINDING_NAME_FISHYQRTOGGLE", "Toggle FishyQR")
+    --#endregion
+end
+
 
 -- INIT -----------------------------
 function FishyQR.OnAddOnLoaded(event, addonName)
@@ -125,141 +264,18 @@ function FishyQR.OnAddOnLoaded(event, addonName)
 
         --load params variable
         FishyQRparams = ZO_SavedVars:NewAccountWide("FishyQRparamsvar", 2, nil, FishyQRdefaults)
-        FishyQR.running = FishyQRparams.run_var
 
         --init chalutier
         fishyChaInit()
 
-        --create qr ui code elements
+        FishyQR.running = FishyQRparams.run_var
 
-        dimX = brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize + brdr
-        dimY = text + brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize + brdr
-
-        FishyQR.UI = WINDOW_MANAGER:CreateControl(nil, GuiRoot, CT_TOPLEVELCONTROL)
-        FishyQR.UI:SetMouseEnabled(true)
-        FishyQR.UI:SetClampedToScreen(true)
-        FishyQR.UI:SetMovable(true)
-        FishyQR.UI:SetDimensions(dimX, dimY)
-        FishyQR.UI:SetDrawLevel(0)
-        FishyQR.UI:SetDrawLayer(DL_MAX_VALUE-1)
-        FishyQR.UI:SetDrawTier(DT_MAX_VALUE-1)
-
-        FishyQR.UI:ClearAnchors()
-        FishyQR.UI:SetAnchor(TOPRIGHT, GuiRoot, TOPRIGHT, FishyQRparams.posx, FishyQRparams.posy)
-
-        FishyQR.UI.background = WINDOW_MANAGER:CreateControl(nil, FishyQR.UI, CT_TEXTURE)
-        FishyQR.UI.background:SetDimensions(dimX, dimY)
-        FishyQR.UI.background:SetColor(1, 1, 1)
-        FishyQR.UI.background:SetAnchor(TOPLEFT, FishyQR.UI, TOPLEFT, 0, 0)
-        FishyQR.UI.background:SetHidden(false)
-        FishyQR.UI.background:SetDrawLevel(0)
-
-        FishyQR.UI.label = WINDOW_MANAGER:CreateControl(FishyQR.name .. "label", FishyQR.UI, CT_LABEL)
-        FishyQR.UI.label:SetFont("ZoFontChat")
-        FishyQR.UI.label:SetColor(0,0,0)
-        FishyQR.UI.label:SetAnchor(TOP, FishyQR.UI.background, TOP, 0, 0)
-        FishyQR.UI.label:SetText("FishyQR")
-
-        FishyQR.UI.buttonLabel = WINDOW_MANAGER:CreateControl(FishyQR.name .. "buttonLabel", FishyQR.UI, CT_LABEL)
-        FishyQR.UI.buttonLabel:SetFont("ZoFontChat")
-        FishyQR.UI.buttonLabel:SetColor(0,0,0)
-        FishyQR.UI.buttonLabel:SetAnchor(TOPRIGHT, FishyQR.UI.background, TOPRIGHT, -brdr, 0)
-
-        FishyQR.UI.button = WINDOW_MANAGER:CreateControl(FishyQR.name .. "button", FishyQR.UI, CT_BUTTON)
-        FishyQR.UI.button:SetDimensions(text, text)
-        FishyQR.UI.button:SetAnchor(TOPRIGHT, FishyQR.UI.background, TOPRIGHT, -brdr, 0)
-        FishyQR.UI.button:SetHandler("OnClicked", FishyQR.toggle_running_state)
-
-        FishyQR.UI.pixel = {}
-        for i = 0,FishyQRparams.maxpixels-1 do
-            FishyQR.UI.pixel[i] = {}
-            for j = 0,FishyQRparams.maxpixels-1 do
-                FishyQR.UI.pixel[i][j] = WINDOW_MANAGER:CreateControl(nil, FishyQR.UI, CT_TEXTURE)
-                FishyQR.UI.pixel[i][j]:SetDimensions(FishyQRparams.pixelsize, FishyQRparams.pixelsize)
-                FishyQR.UI.pixel[i][j]:SetColor(0, 0, 0)
-                FishyQR.UI.pixel[i][j]:SetAnchor(TOPLEFT, FishyQR.UI.background, TOPLEFT, brdr+(i*FishyQRparams.pixelsize), text+brdr+(j*FishyQRparams.pixelsize))
-                FishyQR.UI.pixel[i][j]:SetHidden(true)
-                FishyQR.UI.pixel[i][j]:SetDrawLevel(0)
-            end
-        end
+        _createUI()
+        _createMenu()
 
         _update_state()
-
-        EVENT_MANAGER:RegisterForUpdate(FishyQR.name .. "savePos", 3000, function()
-            FishyQRparams.posy = FishyQR.UI:GetTop()
-            FishyQRparams.posx = FishyQR.UI:GetRight() - GuiRoot:GetRight()
-        end)
-
-        --#region addon menu
-        --addon menu
-        local LAM = LibAddonMenu2
-        local panelName = FishyQR.name .. "Settings"
-
-        local panelData = {
-            type = "panel",
-            name = FishyQR.name .. " Settings",
-            author = FishyQR.author,
-        }
-        local panel = LAM:RegisterAddonPanel(panelName, panelData)
-        local optionsData = {
-            {
-                type = "dropdown",
-                name = "Start State",
-                choices = {"enabled", "disabled"},
-                getFunc = function() if FishyQRparams.run_var then return "enabled" end return "disabled" end,
-                setFunc = function(var)
-                    if var == "enabled" then
-                        FishyQRparams.run_var = true
-                    else
-                        FishyQRparams.run_var = false
-                    end
-                end,
-                tooltip = "If enabled FishyQR will start immediately, when the game is loaded.",
-            },
-            {
-                type = "slider",
-                name = "Pixel Size",
-                min = 1,
-                max = 8,
-                default = 8,
-                getFunc = function() return FishyQRparams.pixelsize end,
-                setFunc = function(value)
-                    FishyQRparams.pixelsize = value
-                    FishyQR.UI:SetDimensions(2*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize, 3*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize + text)
-                    FishyQR.UI.background:SetDimensions(2*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize, 3*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize + text)
-                    for i = 0,FishyQRparams.maxpixels-1 do
-                        for j = 0,FishyQRparams.maxpixels-1 do
-                            FishyQR.UI.pixel[i][j]:SetDimensions(FishyQRparams.pixelsize, FishyQRparams.pixelsize)
-                            FishyQR.UI.pixel[i][j]:SetAnchor(TOPLEFT, FishyQR.UI.background, TOPLEFT, brdr+(i*FishyQRparams.pixelsize), brdr+(j*FishyQRparams.pixelsize))
-                        end
-                    end
-                end,
-                tooltip = "Set the size of each pixel of the QR Code.",
-                requiresReload = true
-            },
-            {
-                type = "slider",
-                name = "Updatetime",
-                min = 150,
-                max = 1500,
-                step = 50,
-                default = 500,
-                getFunc = function() return FishyQRparams.updatetime end,
-                setFunc = function(value) FishyQRparams.updatetime = value end,
-                tooltip = "Set the wait time between each QR Code Update in ms.",
-                requiresReload = true
-            },
-            {
-                type = "description",
-                title = "NOTE",
-                text = "If you experience problems with performance, try increasing Updatetime. The higher the value of Updatetime is, the less it will draw performance.",
-                width = "full"
-            }
-        }
-        LAM:RegisterOptionControls(panelName, optionsData)
-        ZO_CreateStringId("SI_BINDING_NAME_FISHYQRTOGGLE", "Toggle FishyQR")
-        --#endregion
     end
 end
+
 
 EVENT_MANAGER:RegisterForEvent(FishyQR.name, EVENT_ADD_ON_LOADED, FishyQR.OnAddOnLoaded)
