8101fb31689af7e666f480ea9e7689810bf5eab3
SemjonWilke <>
2023-02-11 18:00:18 +0100

    add zip script

---
+++ release.sh
@@ -0,0 +1,7 @@
+rm FishyQR.zip
+mkdir FishyQR
+mkdir FishyQR/luaqrcode
+cp luaqrcode/qrencode.lua FishyQR/luaqrcode/qrencode.lua
+cp FishyQR.txt FishyQR.lua FishyCha.lua Bindings.xml FishyQR
+7z a -r FishyQR.zip FishyQR
+rm -rf FishyQR
