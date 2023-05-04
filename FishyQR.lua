70c64bd1203a2526f39395bbc98150fa0ce16668
SemjonWilke <>
2023-05-04 15:01:59 +0200

    switch to FishingStateMachine

--- FishyQR.lua
+++ FishyQR.lua
@@ -29,6 +29,9 @@ local dimY = 0
 
 --draw qr
 local function _drawQR(keyString)
+    local ui = FishyQR.UI
+    local maxpx = FishyQRparams.maxpixels
+
     local ok, qrtable = qrcode(keyString)
     local tmpLastPixel = 0
     if ok then
@@ -37,34 +40,34 @@ local function _drawQR(keyString)
             tmpLastPixel = i
             for j,val in pairs(ref) do
                 --Error: not enough pixels allocated
-                if j > FishyQRparams.maxpixels then
+                if j > maxpx then
                     return
                 end
 
                 if val < 0 then
-                    FishyQR.UI.pixel[i-1][j-1]:SetHidden(true)
+                    ui.pixel[i-1][j-1]:SetHidden(true)
                 else
-                    FishyQR.UI.pixel[i-1][j-1]:SetHidden(false)
+                    ui.pixel[i-1][j-1]:SetHidden(false)
                 end
             end
         end
 
         --make unused pixels blank
-        if tmpLastPixel < FishyQRparams.maxpixels then
+        if tmpLastPixel < maxpx then
             for i = 1, tmpLastPixel do
-                for j = tmpLastPixel+1, FishyQRparams.maxpixels do
-                    FishyQR.UI.pixel[i-1][j-1]:SetHidden(true)
+                for j = tmpLastPixel+1, maxpx do
+                    ui.pixel[i-1][j-1]:SetHidden(true)
                 end
             end
-            for i = tmpLastPixel+1, FishyQRparams.maxpixels do
-                for j = 1, FishyQRparams.maxpixels do
-                    FishyQR.UI.pixel[i-1][j-1]:SetHidden(true)
+            for i = tmpLastPixel+1, maxpx do
+                for j = 1, maxpx do
+                    ui.pixel[i-1][j-1]:SetHidden(true)
                 end
             end
         end
 
         --resize background
-        FishyQR.UI.background:SetDimensions(dimX, dimY)
+        ui.background:SetDimensions(dimX, dimY)
     end
 end
 
@@ -74,10 +77,10 @@ local function _generateQR()
     --get the gps values and form them to a string
     local x, y, zoneMapIndex = gps:LocalToGlobal(GetMapPlayerPosition("player"))
     local angle = (math.deg(GetPlayerCameraHeading())-180) % 360
-    
+
     -- add all data here
     -- if made changes to this, dont forget to update the parsing in qr_detection._parse_qr_code
-    local keyString = string.format("%f,%f,%d,%d", x, y, angle, FishyQR.engine.currentState)
+    local keyString = string.format("%f,%f,%d,%d", x, y, angle, FishyQR.engine:getState())
 
     --draw QRC with new location
     if tmpKeyString ~= keyString then
@@ -88,40 +91,49 @@ end
 
 
 local function _stopState()
-    FishyQR.UI.buttonLabel:SetText("↓")
-    FishyQR.UI.background:SetDimensions(dimX, text + brdr)
+    local this = FishyQR
+    local ui = this.UI
+    local maxpx = FishyQRparams.maxpixels
 
-    for i = 0,FishyQRparams.maxpixels-1 do
-        for j = 0,FishyQRparams.maxpixels-1 do
-            FishyQR.UI.pixel[i][j]:SetHidden(true)
+    ui.buttonLabel:SetText("↓")
+    ui.background:SetDimensions(dimX, text + brdr)
+
+    for i = 0,maxpx-1 do
+        for j = 0,maxpx-1 do
+            ui.pixel[i][j]:SetHidden(true)
         end
     end
 
-    FishyQR.UI:SetDrawLayer(DL_MIN_VALUE)
-    FishyQR.UI:SetDrawTier(DT_MIN_VALUE)
-    
-    EVENT_MANAGER:UnregisterForUpdate(FishyQR.name .. "generateQR", FishyQRparams.updatetime, _generateQR)
+    ui:SetDrawLayer(DL_MIN_VALUE)
+    ui:SetDrawTier(DT_MIN_VALUE)
+
+    EVENT_MANAGER:UnregisterForUpdate(this.name .. "generateQR")
+    this.engine:unregisterOnStateChange(_generateQR)
 end
 
 
 local function _startState()
-    FishyQR.UI.background:SetDimensions(dimX, dimY)
-    FishyQR.UI.buttonLabel:SetText("—")
+    local this = FishyQR
+    local ui = this.UI
 
-    if FishyQR.UI.pixel ~= nil then
+    ui.background:SetDimensions(dimX, dimY)
+    ui.buttonLabel:SetText("—")
+
+    if ui.pixel ~= nil then
         tmpKeyString = ""
         _generateQR()
     end
-    
-    FishyQR.UI:SetDrawLayer(DL_MAX_VALUE-1)
-    FishyQR.UI:SetDrawTier(DT_MAX_VALUE-1)
 
-    EVENT_MANAGER:RegisterForUpdate(FishyQR.name .. "generateQR", FishyQRparams.updatetime, _generateQR)
+    ui:SetDrawLayer(DL_MAX_VALUE-1)
+    ui:SetDrawTier(DT_MAX_VALUE-1)
+
+    EVENT_MANAGER:RegisterForUpdate(this.name .. "generateQR", FishyQRparams.updatetime, _generateQR)
+    this.engine:registerOnStateChange(_generateQR)
 end
 
 
+--forward declaration due to circular function calls
 local _hideOnSceneChange
-
 
 local function _update_state()
     if FishyQR.running then
@@ -137,9 +149,11 @@ end
 
 
 _hideOnSceneChange = function(oldState, newState)
+    local this = FishyQR
+
     if newState == SCENE_HIDDEN then
-        if FishyQR.engine.currentState < FishyQR.engine.state.depleted or
-           FishyQR.engine.currentState > FishyQR.engine.state.invfull
+        if this.engine:getState() < this.engine.state.depleted or
+           this.engine:getState() > this.engine.state.invfull
         then
             _stopState()
         end
@@ -150,94 +164,106 @@ end
 
 
 local function _enable_on_looking(state)
-    if state == FishyQR.engine.state.looking then
-        FishyQR.engine.CallbackManager:UnregisterCallback(FishyQR.engine.name .. "FishyCha_STATE_CHANGE", _enable_on_looking)
-        FishyQR.running = true
+    local this = FishyQR
+
+    if state == this.engine.state.looking then
+        this.engine:unregisterOnStateChange(_enable_on_looking)
+        this.running = true
         _update_state()
     end
 end
 
 
 function FishyQR.toggle_running_state()
-    FishyQR.running = not FishyQR.running
+    local this = FishyQR
+
+    this.running = not this.running
     _update_state()
 
-    if not FishyQR.running and FishyQRparams.enabled_on_looking then
-        FishyQR.engine.CallbackManager:RegisterCallback(FishyQR.engine.name .. "FishyCha_STATE_CHANGE", _enable_on_looking)
+    if not this.running and FishyQRparams.enabled_on_looking then
+        this.engine:registerOnStateChange(_enable_on_looking)
     end
 end
 
 
-function _createUI()
+local function _createUI()
+    local this = FishyQR
+    this.UI = WINDOW_MANAGER:CreateControl(nil, GuiRoot, CT_TOPLEVELCONTROL)
+    local ui = this.UI
+    local params = FishyQRparams
+
     --create qr ui code elements
-    dimX = brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize + brdr
-    dimY = text + brdr + topPad + FishyQRparams.maxpixels*FishyQRparams.pixelsize + brdr
+    dimX = brdr + params.maxpixels*params.pixelsize + brdr
+    dimY = text + brdr + topPad + params.maxpixels*params.pixelsize + brdr
 
-    FishyQR.UI = WINDOW_MANAGER:CreateControl(nil, GuiRoot, CT_TOPLEVELCONTROL)
-    FishyQR.UI:SetMouseEnabled(true)
-    FishyQR.UI:SetClampedToScreen(true)
-    FishyQR.UI:SetMovable(true)
-    FishyQR.UI:SetDimensions(dimX, dimY)
-    FishyQR.UI:SetDrawLevel(0)
-    FishyQR.UI:SetDrawLayer(DL_MAX_VALUE-1)
-    FishyQR.UI:SetDrawTier(DT_MAX_VALUE-1)
+    ui:SetMouseEnabled(true)
+    ui:SetClampedToScreen(true)
+    ui:SetMovable(true)
+    ui:SetDimensions(dimX, dimY)
+    ui:SetDrawLevel(0)
+    ui:SetDrawLayer(DL_MAX_VALUE-1)
+    ui:SetDrawTier(DT_MAX_VALUE-1)
 
-    FishyQR.UI:ClearAnchors()
-    FishyQR.UI:SetAnchor(TOPRIGHT, GuiRoot, TOPRIGHT, FishyQRparams.posx, FishyQRparams.posy)
+    ui:ClearAnchors()
+    ui:SetAnchor(TOPRIGHT, GuiRoot, TOPRIGHT, params.posx, params.posy)
 
-    FishyQR.UI.background = WINDOW_MANAGER:CreateControl(nil, FishyQR.UI, CT_TEXTURE)
-    FishyQR.UI.background:SetDimensions(dimX, dimY)
-    FishyQR.UI.background:SetColor(1, 1, 1)
-    FishyQR.UI.background:SetAnchor(TOPLEFT, FishyQR.UI, TOPLEFT, 0, 0)
-    FishyQR.UI.background:SetHidden(false)
-    FishyQR.UI.background:SetDrawLevel(0)
+    ui.background = WINDOW_MANAGER:CreateControl(nil, ui, CT_TEXTURE)
+    ui.background:SetDimensions(dimX, dimY)
+    ui.background:SetColor(1, 1, 1)
+    ui.background:SetAnchor(TOPLEFT, ui, TOPLEFT, 0, 0)
+    ui.background:SetHidden(false)
+    ui.background:SetDrawLevel(0)
 
-    FishyQR.UI.label = WINDOW_MANAGER:CreateControl(FishyQR.name .. "label", FishyQR.UI, CT_LABEL)
-    FishyQR.UI.label:SetFont("ZoFontChat")
-    FishyQR.UI.label:SetColor(0,0,0)
-    FishyQR.UI.label:SetAnchor(TOP, FishyQR.UI.background, TOP, 0, 0)
-    FishyQR.UI.label:SetText("FishyQR")
+    ui.label = WINDOW_MANAGER:CreateControl(this.name .. "label", ui, CT_LABEL)
+    ui.label:SetFont("ZoFontChat")
+    ui.label:SetColor(0,0,0)
+    ui.label:SetAnchor(TOP, ui.background, TOP, 0, 0)
+    ui.label:SetText("FishyQR")
 
-    FishyQR.UI.buttonLabel = WINDOW_MANAGER:CreateControl(FishyQR.name .. "buttonLabel", FishyQR.UI, CT_LABEL)
-    FishyQR.UI.buttonLabel:SetFont("ZoFontChat")
-    FishyQR.UI.buttonLabel:SetColor(0,0,0)
-    FishyQR.UI.buttonLabel:SetAnchor(TOPRIGHT, FishyQR.UI.background, TOPRIGHT, -brdr, 0)
+    ui.buttonLabel = WINDOW_MANAGER:CreateControl(this.name .. "buttonLabel", ui, CT_LABEL)
+    ui.buttonLabel:SetFont("ZoFontChat")
+    ui.buttonLabel:SetColor(0,0,0)
+    ui.buttonLabel:SetAnchor(TOPRIGHT, ui.background, TOPRIGHT, -brdr, 0)
 
-    FishyQR.UI.button = WINDOW_MANAGER:CreateControl(FishyQR.name .. "button", FishyQR.UI, CT_BUTTON)
-    FishyQR.UI.button:SetDimensions(text, text)
-    FishyQR.UI.button:SetAnchor(TOPRIGHT, FishyQR.UI.background, TOPRIGHT, -brdr, 0)
-    FishyQR.UI.button:SetHandler("OnClicked", FishyQR.toggle_running_state)
+    ui.button = WINDOW_MANAGER:CreateControl(this.name .. "button", ui, CT_BUTTON)
+    ui.button:SetDimensions(text, text)
+    ui.button:SetAnchor(TOPRIGHT, ui.background, TOPRIGHT, -brdr, 0)
+    ui.button:SetHandler("OnClicked", FishyQR.toggle_running_state)
 
-    FishyQR.UI.pixel = {}
-    for i = 0,FishyQRparams.maxpixels-1 do
-        FishyQR.UI.pixel[i] = {}
-        for j = 0,FishyQRparams.maxpixels-1 do
-            FishyQR.UI.pixel[i][j] = WINDOW_MANAGER:CreateControl(nil, FishyQR.UI, CT_TEXTURE)
-            FishyQR.UI.pixel[i][j]:SetDimensions(FishyQRparams.pixelsize, FishyQRparams.pixelsize)
-            FishyQR.UI.pixel[i][j]:SetColor(0, 0, 0)
-            FishyQR.UI.pixel[i][j]:SetAnchor(TOPLEFT, FishyQR.UI.background, TOPLEFT, brdr+(i*FishyQRparams.pixelsize), text+brdr+topPad+(j*FishyQRparams.pixelsize))
-            FishyQR.UI.pixel[i][j]:SetHidden(true)
-            FishyQR.UI.pixel[i][j]:SetDrawLevel(0)
+    ui.pixel = {}
+    for i = 0,params.maxpixels-1 do
+        ui.pixel[i] = {}
+        for j = 0,params.maxpixels-1 do
+            ui.pixel[i][j] = WINDOW_MANAGER:CreateControl(nil, ui, CT_TEXTURE)
+            ui.pixel[i][j]:SetDimensions(params.pixelsize, params.pixelsize)
+            ui.pixel[i][j]:SetColor(0, 0, 0)
+            ui.pixel[i][j]:SetAnchor(TOPLEFT, ui.background, TOPLEFT, brdr+(i*params.pixelsize), text+brdr+topPad+(j*params.pixelsize))
+            ui.pixel[i][j]:SetHidden(true)
+            ui.pixel[i][j]:SetDrawLevel(0)
         end
     end
 
-    FishyQR.UI:SetHandler("OnMoveStop", function()
-        FishyQRparams.posy = FishyQR.UI:GetTop()
-        FishyQRparams.posx = FishyQR.UI:GetRight() - GuiRoot:GetRight()
+    ui:SetHandler("OnMoveStop", function()
+        params.posy = ui:GetTop()
+        params.posx = ui:GetRight() - GuiRoot:GetRight()
     end)
 end
 
 
 function _createMenu()
+    local this = FishyQR
+    local ui = this.UI
+    local params = FishyQRparams
+
     --#region addon menu
     --addon menu
     local LAM = LibAddonMenu2
-    local panelName = FishyQR.name .. "Settings"
+    local panelName = this.name .. "Settings"
 
     local panelData = {
         type = "panel",
-        name = FishyQR.name,
-        author = FishyQR.author,
+        name = this.name,
+        author = this.author,
     }
     local panel = LAM:RegisterAddonPanel(panelName, panelData)
     local optionsData = {
@@ -245,22 +271,25 @@ function _createMenu()
             type = "checkbox",
             name = "Start State",
             default = true,
-            getFunc = function() return FishyQRparams.run_var end,
-            setFunc = function(value) FishyQRparams.run_var = value end,
+            getFunc = function() return params.run_var end,
+            setFunc = function(value) params.run_var = value end,
             tooltip = "If enabled FishyQR will start immediately, when the game is loaded.",
         },
         {
             type = "checkbox",
             name = "Hide On Scene Change",
             default = true,
-            getFunc = function() return FishyQRparams.change_scene end,
+            getFunc = function() return params.change_scene end,
             setFunc = function(value)
-                FishyQRparams.change_scene = value
+                params.change_scene = value
                 if value then
                     HUD_SCENE:RegisterCallback("StateChange", _hideOnSceneChange)
+                    this.running = false
                 else
                     HUD_SCENE:UnregisterCallback("StateChange", _hideOnSceneChange)
+                    this.running = true
                 end
+                _update_state()
             end,
             tooltip = "If enabled FishyQR will hide when a menu is opened.",
         },
@@ -268,13 +297,13 @@ function _createMenu()
             type = "checkbox",
             name = "Enable when looking at a fishing hole",
             default = true,
-            getFunc = function() return FishyQRparams.enabled_on_looking end,
+            getFunc = function() return params.enabled_on_looking end,
             setFunc = function(value)
-                FishyQRparams.enabled_on_looking = value
+                params.enabled_on_looking = value
                 if value then
-                    FishyQR.engine.CallbackManager:RegisterCallback(FishyQR.engine.name .. "FishyCha_STATE_CHANGE", _enable_on_looking)
+                    this.engine:registerOnStateChange(_enable_on_looking)
                 else
-                    FishyQR.engine.CallbackManager:UnregisterCallback(FishyQR.engine.name .. "FishyCha_STATE_CHANGE", _enable_on_looking)
+                    this.engine:unregisterOnStateChange(_enable_on_looking)
                 end
             end,
             tooltip = "If enabled FishyQR will automatically start when the player is looking at a fishing hole.",
@@ -283,34 +312,40 @@ function _createMenu()
             type = "slider",
             name = "Pixel Size",
             min = 1,
-            max = 8,
+            max = 16,
             default = 8,
-            getFunc = function() return FishyQRparams.pixelsize end,
+            getFunc = function() return params.pixelsize end,
             setFunc = function(value)
-                FishyQRparams.pixelsize = value
-                FishyQR.UI:SetDimensions(2*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize, 3*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize + text)
-                FishyQR.UI.background:SetDimensions(2*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize, 3*brdr + FishyQRparams.maxpixels*FishyQRparams.pixelsize + text)
-                for i = 0,FishyQRparams.maxpixels-1 do
-                    for j = 0,FishyQRparams.maxpixels-1 do
-                        FishyQR.UI.pixel[i][j]:SetDimensions(FishyQRparams.pixelsize, FishyQRparams.pixelsize)
-                        FishyQR.UI.pixel[i][j]:SetAnchor(TOPLEFT, FishyQR.UI.background, TOPLEFT, brdr+(i*FishyQRparams.pixelsize), brdr+(j*FishyQRparams.pixelsize))
+                params.pixelsize = value
+                dimX = brdr + params.maxpixels*params.pixelsize + brdr
+                dimY = text + brdr + topPad + params.maxpixels*params.pixelsize + brdr
+                ui:SetDimensions(dimX, dimY)
+                ui.background:SetDimensions(dimX, dimY)
+                for i = 0,params.maxpixels-1 do
+                    for j = 0,params.maxpixels-1 do
+                        ui.pixel[i][j]:SetDimensions(params.pixelsize, params.pixelsize)
+                        ui.pixel[i][j]:SetAnchor(TOPLEFT, ui.background, TOPLEFT, brdr+(i*params.pixelsize), brdr+(j*params.pixelsize)+text+topPad)
                     end
                 end
             end,
-            tooltip = "Set the size of each pixel of the QR Code.",
-            requiresReload = true
+            tooltip = "Set the size of each pixel of the QR Code."
         },
         {
             type = "slider",
             name = "Updatetime",
-            min = 150,
+            min = 50,
             max = 1500,
             step = 50,
             default = 500,
-            getFunc = function() return FishyQRparams.updatetime end,
-            setFunc = function(value) FishyQRparams.updatetime = value end,
-            tooltip = "Set the wait time between each QR Code Update in ms.",
-            requiresReload = true
+            getFunc = function() return params.updatetime end,
+            setFunc = function(value)
+                EVENT_MANAGER:UnregisterForUpdate(this.name .. "generateQR")
+                params.updatetime = value
+                if this.running then
+                    EVENT_MANAGER:RegisterForUpdate(this.name .. "generateQR", value, _generateQR)
+                end
+            end,
+            tooltip = "Set the wait time between each QR Code Update in ms."
         },
         {
             type = "description",
@@ -326,7 +361,7 @@ end
 
 
 -- INIT -----------------------------
-function FishyQR.OnAddOnLoaded(event, addonName)
+local function _onAddOnLoaded(event, addonName)
     if addonName == FishyQR.name then
         --init once and never come here again
         EVENT_MANAGER:UnregisterForEvent(FishyQR.name, EVENT_ADD_ON_LOADED)
@@ -335,8 +370,7 @@ function FishyQR.OnAddOnLoaded(event, addonName)
         FishyQRparams = ZO_SavedVars:NewAccountWide("FishyQRparamsvar", 2, nil, FishyQRdefaults)
 
         --init chalutier
-        fishyChaInit()
-        FishyQR.engine = FishyCha
+        FishyQR.engine = FishingStateMachine
 
         FishyQR.running = FishyQRparams.run_var
 
@@ -344,7 +378,7 @@ function FishyQR.OnAddOnLoaded(event, addonName)
         _createMenu()
 
         if FishyQRparams.enabled_on_looking then
-            FishyQR.engine.CallbackManager:RegisterCallback(FishyQR.engine.name .. "FishyCha_STATE_CHANGE", _enable_on_looking)
+            FishyQR.engine:registerOnStateChange(_enable_on_looking)
         end
 
         _update_state()
@@ -352,4 +386,4 @@ function FishyQR.OnAddOnLoaded(event, addonName)
 end
 
 
-EVENT_MANAGER:RegisterForEvent(FishyQR.name, EVENT_ADD_ON_LOADED, FishyQR.OnAddOnLoaded)
+EVENT_MANAGER:RegisterForEvent(FishyQR.name, EVENT_ADD_ON_LOADED, _onAddOnLoaded)
