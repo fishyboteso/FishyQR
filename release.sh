4f6b4652809c17dd5b8ef9ba0789ffca0a874074
Semjon Wilke <>
2023-08-01 23:16:53 +0200

    add version to script

--- release.sh
+++ release.sh
@@ -1,7 +1,8 @@
-rm FishyQR.zip
+VERSION="$(cat FishyQR.txt  | grep "## Version:" | cut -d":" -f2 | xargs)"
+rm FishyQR*.zip
 mkdir FishyQR
 mkdir FishyQR/luaqrcode
 cp luaqrcode/qrencode.lua FishyQR/luaqrcode/qrencode.lua
 cp FishyQR.txt FishyQR.lua Bindings.xml FishyQR
-7z a -r FishyQR.zip FishyQR
+7z a -r FishyQR-$VERSION.zip FishyQR
 rm -rf FishyQR
