23edb8909ab4ff93d46de2988bc74ce8044b593b
Semjon Wilke <>
2023-08-04 21:06:34 +0200

    add logger check to release script

--- release.sh
+++ release.sh
@@ -1,3 +1,9 @@
+for f in $(find . -name "*.lua"); do
+    if [ $(cat $f | grep logger | grep -v "^ *--" | wc -l) -gt 0 ]; then
+        echo -e "Attention! There are loggers in the source code\n"
+        read
+    fi
+done
 VERSION="$(cat FishyQR.txt  | grep "## Version:" | cut -d":" -f2 | xargs)"
 rm FishyQR*.zip
 mkdir FishyQR
